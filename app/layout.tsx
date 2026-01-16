4dcc8f17bf5594afd067682e642b71bce7262f17
Gerald Hill <111713369+geralddhill@users.noreply.github.com>
2026-01-16 00:11:04 -0800

    Made landing page a flexbox

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     return (
     <html lang="en" className={`${playfair.variable} ${barlow.variable} ${poppins.variable}`}>
       <body
-        className={`antialiased w-full`}
+        className={`antialiased w-full flex flex-col justify-between min-h-screen`}
       >
         <Header/>
         {children}
--- ui/Footer.tsx
+++ ui/Footer.tsx
@@ -3,7 +3,7 @@ import SocialButton from "@/ui/SocialButton";
 import {cn} from "@/lib/utils";
 
 export default function Footer() {
-    return (<footer className="w-full absolute bottom-0 bg-background2 flex flex-col items-center justify-center">
+    return (<footer className="w-full bg-background2 flex flex-col items-center justify-center">
         <h4 className="text-3xl m-4">Gerald Hill</h4>
         <div className="flex items-center justify-center space-x-4 h-8 m-2">
             <SocialButton href="https://github.com/geralddhill">
